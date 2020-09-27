2ddbc5bbbd06daca5a0ec32720303ce73035f625
Scott Sirri <scottsirri@scotts-mbp.dhcp.wustl.edu>
2020-09-26 20:23:44 -0500

    Trying to implement not retaining surplus spaces at line beginnings when many consecutive spaces are entered

--- fold.c
+++ fold.c
@@ -3,27 +3,36 @@
 #define LINEWIDTH 20
 #define MAXLENGTH 500
 
+char * copyToLength(char *, char *, int *);
+
 int main(int argc, char * argv[]) {
 	int inputLength;
-	char * input = copyToLength(argv[1], &inputLength, MAXLENGTH);
-
+	char input[MAXLENGTH];
+	copyToLength(argv[1], input, &inputLength);
+	
 	int numLines = 0;
 	for(int i = 0; input[i] != 0; i++) {
 		if(i == (numLines + 1) * LINEWIDTH) {
-			for(;input[i] == ' ' || input[i] == '\t'; --i) ;	// After this line, i is the index of the last nonblank character before any spaces (after which should be inserted '\n')
-			for(int rightShiftIndex = inputLength + 1; rightShiftIndex > i; --rightShiftIndex) input[rightShiftIndex] = input[rightShiftIndex - 1]; 
-			input[i + 1] = '\n';
+			while(input[i] != ' ' && input[i] != '\t') --i;	// After this line, i is the index of the last nonblank character before any spaces (after which should be inserted '\n')
+			input[i]='\n';
+			int lastBlank = i;
+			while(input[lastBlank + 1] == ' ' || input[lastBlank + 1] == 't') lastBlank++;
+			if(lastBlank > i) {
+				for(int rightShiftIndex = inputLength - (lastBlank - i); rightShiftIndex > i; --rightShiftIndex) input[rightShiftIndex] = input[rightShiftIndex + (lastBlank - i)];
+			 	for(int setNullIndex = inputLength - (lastBlank - i) + 1; setNullIndex < inputLength; ++setNullIndex) input[setNullIndex] = 0;
+				inputLength -= (lastblank - i)
+			}
 			++numLines;
 		}
 	}
 	printf("%s\n", input);
 }
 
-int * copyToLength(char * array, int * outputLength, int maxLength) {
-	char * out[maxLength];
+char * copyToLength(char * array, char * out, int * outputLength) {
 	int index  = 0;
 	for(; array[index] != 0; ++index) out[index] = array[index];
 	out[index] = 0;
 	*outputLength = index;
+	//printf("outputLength: %d\n", *outputLength);
 	return out;
 }
