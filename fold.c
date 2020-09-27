e37a0bb2815df26645a27ddfd7008fc858cf221d
Scott Sirri <scottsirri@scotts-mbp.dhcp.wustl.edu>
2020-09-26 21:39:50 -0500

    FOLDING IS ALIIIIIIVE. Seems to be functional, including CML parameters/input. Requires documentation

--- fold.c
+++ fold.c
@@ -1,31 +1,47 @@
 #include <stdio.h>
 
-#define LINEWIDTH 20
 #define MAXLENGTH 500
 
 char * copyToLength(char *, char *, int *);
 
+
 int main(int argc, char * argv[]) {
+	char LINEWIDTH = 12;
+	if(argc == 3) {
+		LINEWIDTH = 0;
+		for(int i = 0; *(argv[2] + i) != 0; ++i) {
+			LINEWIDTH *= 10;
+			LINEWIDTH += *(argv[2] + i) - 48;
+		}
+	}
+	printf("LINEWIDTH: %d\n", LINEWIDTH);
 	int inputLength;
 	char input[MAXLENGTH];
 	copyToLength(argv[1], input, &inputLength);
 	
+	int lineStartIndex = 0;
 	int numLines = 0;
 	for(int i = 0; input[i] != 0; i++) {
-		if(i == (numLines + 1) * LINEWIDTH) {
-			while(input[i] != ' ' && input[i] != '\t') --i;	// After this line, i is the index of the last nonblank character before any spaces (after which should be inserted '\n')
-			input[i]='\n';
+		if(i - lineStartIndex >= LINEWIDTH) {
+			// Decrement i while input[i] isn't a blank char preceded by a non-blank char
+			while((input[i - 1] == ' ' || input[i - 1] == '\t') || (input[i] != ' ' && input[i] != '\t')) --i;	// After this line, i is the index of the last blank character before any spaces (after which should be inserted '\n')
+			input[i] = '\n';
+			lineStartIndex = i + 1;
 			int lastBlank = i;
-			while(input[lastBlank + 1] == ' ' || input[lastBlank + 1] == 't') lastBlank++;
+			while(input[lastBlank + 1] == ' ' || input[lastBlank + 1] == '\t') ++lastBlank;
+			int numExtraSpaces;
 			if(lastBlank > i) {
-				for(int rightShiftIndex = inputLength - (lastBlank - i); rightShiftIndex > i; --rightShiftIndex) input[rightShiftIndex] = input[rightShiftIndex + (lastBlank - i)];
-			 	for(int setNullIndex = inputLength - (lastBlank - i) + 1; setNullIndex < inputLength; ++setNullIndex) input[setNullIndex] = 0;
-				inputLength -= (lastblank - i)
+				numExtraSpaces = lastBlank - i;
+				for(int rightShiftIndex = i + 1; rightShiftIndex < inputLength - numExtraSpaces; ++rightShiftIndex) {
+					input[rightShiftIndex] = input[rightShiftIndex + numExtraSpaces];
+				}
+				input[inputLength - numExtraSpaces] = 0;
+				inputLength -= numExtraSpaces;
 			}
 			++numLines;
 		}
 	}
-	printf("%s\n", input);
+	printf("\n%s\n", input);
 }
 
 char * copyToLength(char * array, char * out, int * outputLength) {
@@ -33,6 +49,5 @@ char * copyToLength(char * array, char * out, int * outputLength) {
 	for(; array[index] != 0; ++index) out[index] = array[index];
 	out[index] = 0;
 	*outputLength = index;
-	//printf("outputLength: %d\n", *outputLength);
 	return out;
 }
